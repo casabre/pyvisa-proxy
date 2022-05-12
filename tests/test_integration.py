9c674d2203f596f6a7e2f8890bf0bad1cd4baad6
Carsten Sauerbrey <carsten.sauerbrey@gmail.com>
2022-05-12 22:38:22 +0200

    Updating skip version for integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -10,7 +10,7 @@ from pyvisa_proxy.ProxyResource import ProxyResource
 def test_integration_with_class(
     sync_port, resource_name, executor, rm_sim, idn_string, query_string
 ):
-    if parse(pyvisa.__version__) <= parse("1.11.4"):
+    if parse(pyvisa.__version__) < parse("1.12.0"):
         pytest.skip("PyVISA-proxy implementation in PyVISA is missing.")
 
     with ProxyServer(sync_port, "@sim") as server:
@@ -23,7 +23,7 @@ def test_integration_with_class(
 def test_integration_with_main(
     sync_port, resource_name, executor, rm_sim, idn_string, query_string
 ):
-    if parse(pyvisa.__version__) <= parse("1.11.4"):
+    if parse(pyvisa.__version__) < parse("1.12.0"):
         pytest.skip("PyVISA-proxy implementation in PyVISA is missing.")
     executor.submit(main, sync_port, "@sim")
     get_resource_and_test(
