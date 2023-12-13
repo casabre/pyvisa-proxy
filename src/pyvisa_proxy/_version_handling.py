49f4e88f5146b74d77f7f3593adc93a014c01c3f
Carsten <carsten.sauerbrey@gmail.com>
2023-12-13 12:04:03 +0100

    Removing Python 3.7 support and adding Python 3.11

--- src/pyvisa_proxy/_version_handling.py
+++ src/pyvisa_proxy/_version_handling.py
@@ -5,10 +5,7 @@
 """
 import sys
 
-if sys.version_info >= (3, 8):
-    from importlib.metadata import PackageNotFoundError, version
-else:
-    from importlib_metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError, version
 
 
 def get_version():
